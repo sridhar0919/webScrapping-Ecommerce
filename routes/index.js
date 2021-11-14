dfded2bec019782ca98aafab7ff7e1d37e255242
sridhar0919 <sridhar9797@gmail.com>
2021-11-14 16:40:16 +0530

    Second commit

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,33 @@ var router = express.Router();
 const { dbUrl, mongodb, MongoClient } = require('../dbConfig');
 const { getDetails } = require('../library/scraping');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET details */
+router.get('/', async (req, res) => {
+  const client = await MongoClient.connect(dbUrl);
+
+  try {
+    const db = client.db('webScrappingEcommerce');
+    let data = await db.collection('flipkartProductDetails').find().toArray();
+    res.send({
+      message: 'OK',
+      details: data,
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({
+      message: 'Error in connection',
+    });
+  } finally {
+    client.close();
+  }
 });
 
+// Post check
 router.post('/', (req, res) => {
   res.send({ message: 'Job created successfully' });
 });
+
+// Posting all product detailss
 
 router.post('/add-details-flipkart', async (req, res) => {
   const client = await MongoClient.connect(dbUrl);
